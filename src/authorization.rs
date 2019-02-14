6bc9f361d066a1dc674ceca0690b4e8cdb033413
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-02-14 10:09:56 +0800

    Refactor some stuffs

--- src/authorization.rs
+++ src/authorization.rs
@@ -1,10 +1,10 @@
 use select::document::Document;
 use select::predicate::Attr;
 use std::collections::HashMap;
-use reqwest::{Client, RedirectPolicy, RequestBuilder, Response};
-use reqwest::header::{HeaderValue, CONTENT_LENGTH, CONTENT_TYPE, COOKIE, LOCATION, SET_COOKIE};
+use reqwest::{Client, Method, RedirectPolicy, RequestBuilder, Response};
+use reqwest::header::{HeaderValue, COOKIE, LOCATION, SET_COOKIE};
 use url::Url;
-use serde::{Serialize, Deserialize};
+use serde::Deserialize;
 
 const AUTH_BASE_URL: &'static str = "https://luminus.nus.edu.sg";
 const DISCOVERY_PATH: &'static str = "/v2/auth/.well-known/openid-configuration";
@@ -12,6 +12,8 @@ const CLIENT_ID: &'static str = "verso";
 const SCOPE: &'static str = "profile email role openid lms.read calendar.read lms.delete lms.write calendar.write gradebook.write offline_access";
 const RESPONSE_TYPE: &'static str = "id_token token code";
 const REDIRECT_URI: &'static str = "https://luminus.nus.edu.sg/auth/callback";
+const API_BASE_URL: &'static str = "https://luminus.azure-api.net";
+const OCM_APIM_SUBSCRIPTION_KEY: &'static str = "6963c200ca9440de8fa1eede730d8f7e";
 
 #[derive(Deserialize)]
 struct Discovery {
@@ -75,7 +77,6 @@ fn build_client() -> Result<Client, &'static str> {
 pub fn generate_random_bytes(size: usize) -> String {
     (0..size).map(|_| format!("{:02x}", rand::random::<u8>())).collect()
 }
-
 fn get_redirect_url(response: Response) -> Result<Url, &'static str> {
     let location = response.headers().get(LOCATION).ok_or("Invalid response from server, expected redirection")?
         .to_str().map_err(|_| "Unable to read location header")?.to_string();
@@ -88,19 +89,21 @@ impl Authorization {
         Authorization { jwt: None, cookies: HashMap::new() }
     }
 
-    fn http_post<T: Serialize + ?Sized>(&mut self, url: Url, query: &T) -> Result<Response, &'static str> {
-        let client = build_client()?;
-        let response = self.add_cookie_header(client.post(url)).form(query).send().map_err(|_|"Failed HTTP request")?;
-        for c in response.headers().get_all(SET_COOKIE).iter() {
-            let cookie = c.to_str().map_err(|_| "Unable to read set-cookie header")?.to_string();
-            self.add_cookie(cookie);
-        }
-        Ok(response)
+    fn auth_http_post(&mut self, url: Url, query: &HashMap<&str, &str>) -> Result<Response, &'static str> {
+        return self.auth_http_request(Method::POST, url, Some(query));
     }
 
-    fn http_get(&mut self, url: Url) -> Result<Response, &'static str> {
+    fn auth_http_get(&mut self, url: Url) -> Result<Response, &'static str> {
+        return self.auth_http_request(Method::GET, url, None);
+    }
+
+    fn auth_http_request(&mut self, method: Method, url: Url, form: Option<&HashMap<&str, &str>>) -> Result<Response, &'static str> {
         let client = build_client()?;
-        let response = self.add_cookie_header(client.get(url)).send().map_err(|_| "Failed HTTP request")?;
+        let mut request_builder = self.add_cookie_header(client.request(method, url));
+        if let Some(form) = form {
+            request_builder = request_builder.form(form);
+        }
+        let response = request_builder.send().map_err(|_| "Failed HTTP request")?;
         for c in response.headers().get_all(SET_COOKIE).iter() {
             let cookie = c.to_str().map_err(|_| "Unable to read set-cookie header")?.to_string();
             self.add_cookie(cookie);
@@ -112,12 +115,12 @@ impl Authorization {
         let login_info = self.auth_login_info()?;
         let url = full_auth_url(&login_info.login_url);
         let params = login_info.anti_forgery.build_login_params(username, password);
-        let first_response = self.http_post(url, &params)?;
+        let first_response = self.auth_http_post(url, &params)?;
         if !first_response.status().is_redirection() {
             return Err("Invalid credentials");
         }
         let second_url = get_redirect_url(first_response)?;
-        let callback_url = get_redirect_url(self.http_get(second_url)?)?;
+        let callback_url = get_redirect_url(self.auth_http_get(second_url)?)?;
         return self.handle_callback(callback_url);
     }
 
@@ -126,8 +129,7 @@ impl Authorization {
             return Err("Please login first.")
         }
         let auth_url = auth_endpoint_uri();
-        let callback_url = get_redirect_url(self.http_get(auth_url)?)?;
-        println!("{}", &callback_url);
+        let callback_url = get_redirect_url(self.auth_http_get(auth_url)?)?;
         return self.handle_callback(callback_url);
     }
 
@@ -144,8 +146,8 @@ impl Authorization {
 
     fn auth_login_info(&mut self) -> Result<LoginInfo, &'static str> {
         let auth_url = auth_endpoint_uri();
-        let second_url = get_redirect_url(self.http_get(auth_url)?)?;
-        let second_body = self.http_get(second_url)?.text().map_err(|_| "Unable to read HTTP response body")?;
+        let second_url = get_redirect_url(self.auth_http_get(auth_url)?)?;
+        let second_body = self.auth_http_get(second_url)?.text().map_err(|_| "Unable to read HTTP response body")?;
         let raw_json = Document::from(second_body.as_str()).find(Attr("id", "modelJson")).last().ok_or("No JSON was sent")?.text().trim().to_owned();
         let json = htmlescape::decode_html(&raw_json).map_err(|_| "Unable to decode HTML entities")?;
         let login_info: LoginInfo = serde_json::from_str(&json).map_err(|_| "Unable to decode JSON")?;
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,13 @@ use std::io::Write;
 
 fn main() {
     let mut username = String::new();
-    let mut password = String::new();
     print!("Username: ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut username);
+    io::stdin().read_line(&mut username).expect("Unable to get input");
+    username = username.trim().to_string();
     print!("Password: ");
     io::stdout().flush().unwrap();
-    password = rpassword::read_password().expect("Unable to get non-echo input mode for password");
-    username = username.trim().to_string();
-    password = password.trim().to_string();
+    let password = rpassword::read_password().expect("Unable to get non-echo input mode for password");
     let mut auth = Authorization::new();
     match auth.login(&username, &password) {
         Ok(_) => {

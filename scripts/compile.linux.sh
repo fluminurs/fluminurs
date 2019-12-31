c618907098f83bed14055e8ab680bd4dd4bc5561
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-12-31 18:35:07 +0800

    Re-order cargo bloat

--- scripts/compile.linux.sh
+++ scripts/compile.linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cargo install cargo-bloat
 cargo build --release
+cargo bloat --release
 strip -s target/release/fluminurs
 cp target/release/fluminurs fluminurs.linux
-cargo bloat --release
--- scripts/compile.windows.sh
+++ scripts/compile.windows.sh
@@ -1,5 +1,5 @@
 cargo install cargo-bloat
 cargo build --release
+cargo bloat --release
 strip -s target/release/fluminurs
 cp target/release/fluminurs fluminurs.windows.exe
-cargo bloat --release

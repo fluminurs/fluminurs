8c0c05460f268eb87e507b00bd4e960dadb1e81a
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-12-31 18:55:47 +0800

    Fix windows build script

--- scripts/compile.windows.sh
+++ scripts/compile.windows.sh
@@ -1,5 +1,5 @@
 cargo install cargo-bloat
 cargo build --release
 cargo bloat --release
-strip -s target/release/fluminurs
+strip -s target/release/fluminurs.exe
 cp target/release/fluminurs fluminurs.windows.exe

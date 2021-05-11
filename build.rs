442fa74318b4d50ae05930656c5270c29e15cad7
Bernard Teo <btzy1996@hotmail.com>
2021-05-12 00:27:10 +0800

    Add Windows manifest to enable long paths (#649)
    While we could have used a target-specific build dependency, it confuses Cargo.lock. So for now we force all OSes to download the winres crate (but the stuff in the crate should probably be no-ops on non-Windows). Resolves #646 to the extent where possible. Users still need to opt-in to the new behaviour by setting a registry key (https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation).

---
+++ build.rs
@@ -0,0 +1,20 @@
+use winres;
+
+fn main() -> std::io::Result<()> {
+    if cfg!(target_os = "windows") {
+        // We need to set the 'longPathAware' manifest key, so that file paths with length >260 chars will work.
+        // This happens sometimes since we encode IDs for duplicate files.
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest(
+            r#"<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0" xmlns:asmv3="urn:schemas-microsoft-com:asm.v3">
+<application xmlns="urn:schemas-microsoft-com:asm.v3">
+    <windowsSettings xmlns:ws2="http://schemas.microsoft.com/SMI/2016/WindowsSettings">
+        <ws2:longPathAware>true</ws2:longPathAware>
+    </windowsSettings>
+</application>
+</assembly>"#);
+        res.compile()?;
+    }
+    Ok(())
+}
